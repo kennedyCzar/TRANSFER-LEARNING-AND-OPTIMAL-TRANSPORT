64c2555edd15a6f42fb5b290614cded562a4e74d
kennedy <kennyjunior022006@yahoo.com>
2019-11-19 09:30:45 +0100

    Updated subspace alignment script
    Updated subspace alignment script

--- transferlearning.py
+++ transferlearning.py
@@ -9,6 +9,7 @@ Created on Mon Nov 18 14:13:47 2019
 
 from __future__ import absolute_import
 import os
+import warnings
 import numpy as np
 from scipy.io import loadmat
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
@@ -16,7 +17,8 @@ from KPCA import kPCA
 from sklearn.neighbors import KNeighborsClassifier
 from PCA import PCA
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score
+from utils import EvalC
+from sklearn.exceptions import DataConversionWarning
 
 path = '/home/kenneth/Documents/MLDM M2/ADVANCE_ML/TRANSFER LEARNING/DATASET'
 data = loadmat(os.path.join(path, 'webcam.mat'))
@@ -29,16 +31,17 @@ y_d = data['labels']
 
 #%%
 
-class subspacealignment(object):
+class subspacealignment(EvalC):
     def __init__(self):
         '''
-        Domain Adaptation
+        Domain Adaptation via Subspace alignment
         :References: https://hal.archives-ouvertes.fr/hal-00869417/document
                     https://arxiv.org/abs/1409.5241
         '''
+        super().__init__()
         return
     
-    def fit(self, ds_x = None, ds_y = None, dt_x = None, \
+    def fit_predict(self, ds_x = None, ds_y = None, dt_x = None, \
             dt_y = None, d = None, type = None, m_kernel = None):
         '''Domain Adaptation using Subspace Alignment
         :param: ds_x: NxD
@@ -54,7 +57,7 @@ class subspacealignment(object):
         if ds_y is None:
             raise IOError('Source Input data in required')
         else:
-            self.ds_y = ds_y
+            self.ds_y = ds_y.ravel()
         if dt_x is None:
             raise IOError('Source Input data in required')
         else:
@@ -62,7 +65,7 @@ class subspacealignment(object):
         if dt_y is None:
             raise IOError('Source Input data in required')
         else:
-            self.dt_y = dt_y
+            self.dt_y = dt_y.ravel()
         if d is None:
             d = 2
             self.d = d
@@ -73,8 +76,10 @@ class subspacealignment(object):
             self.m_kernel = m_kernel
         else:
             self.m_kernel = m_kernel
+        #ignore warning when scaling data using MinMaxScaler
+        warnings.filterwarnings('ignore', category = DataConversionWarning)
         #find PCA for Source domain after scaling
-        X_w = MinMaxScaler().fit_transform(self.ds_x) #scale source data
+        X_w = MinMaxScaler().fit_transform(self.ds_x).astype(float) #scale source data
         if not type:
             X_s = kPCA(k = self.d, kernel = self.m_kernel).fit(X_w.T) #perform PCA
         else:
@@ -82,7 +87,7 @@ class subspacealignment(object):
         X_s = X_s.components_.T #get components
         
         #PCA for target domain after scaling
-        X_d = MinMaxScaler().fit_transform(self.dt_x) #scale target data
+        X_d = MinMaxScaler().fit_transform(self.dt_x).astype(float) #scale target data
         if not type:
             X_t = kPCA(k = self.d, kernel = self.m_kernel).fit(X_d.T) #perform PCA
         else:
@@ -93,21 +98,25 @@ class subspacealignment(object):
         X_a = X_s.dot(X_s.T.dot(X_t))
         S_a = self.ds_x.dot(X_a) #source projection
         T_a = self.dt_x.dot(X_t) #target projection
-        
+        print('>>>> Done with PCA and Data projection >>>>')
         #perform classification
         '''
         Fit a 1-NN classifier on S_a and make predictions on T_a
         '''
+        print('*'*40)
+        print('Initializing 1-Nearest Neighbour classifier')
         self.classifier = KNeighborsClassifier(n_neighbors = 1)
         self.classifier.fit(S_a, self.ds_y)
-        self.accuracy = accuracy_score(self.dt_y, self.classifier.predict(T_a))
-        return self.accuracy
+        print('>>>> Done fitting source domain >>>>')
+        self.ypred = self.classifier.predict(T_a)
+        print(EvalC.summary(self.dt_y, self.ypred))
+        return self
         
 
 
 #%% Testing
         
-subalignacc = subspacealignment().fit(X_w, y_w, X_d, y_d, d = 10, m_kernel = 'linear')
+subalignacc = subspacealignment().fit_predict(X_w, y_w, X_d, y_d, d = 10, m_kernel = 'rbf')
 
 
 
